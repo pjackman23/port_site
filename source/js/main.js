47c345557ae577fd4a74fc4417011ec4db844811
pjackman <pjackman23@gmail.com>
2014-11-30 22:50:35 -0600

    Initial Layout Complete
    Header styled, body set up.

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,34 @@
+//event functions
 
+//nav img
+document.getElementById('nav-img-a').onmouseover=function () {
+    document.getElementById('nav-img').src = "../img/logo-invert.png";
+};
+
+document.getElementById('nav-img-a').onmouseout=function () {
+    document.getElementById('nav-img').src = "../img/logo-default.png";
+};
+
+document.getElementById('nav-img-a').onmousedown=function () {
+    document.getElementById('nav-img').src = "../img/logo-glow.png";
+};
+
+document.getElementById('nav-img-a').onmouseup=function () {
+    document.getElementById('nav-img').src = "../img/logo-invert.png";
+};
+
+//nav scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
